92ea1e847de9cf238c544af9c931c7ba89a3742a
neurojojo <joemesz@gmail.com>
2020-09-27 12:43:14 -0400

    After indenting

--- matlab_analyzer.py
+++ matlab_analyzer.py
@@ -3,8 +3,8 @@ def dissect_mFile( mfile_address ):
 	functions = list()
 	filename = re.search( '(.*\/)([^/]*(?=\.))', mfile_address ).group(2)
 	with open( mfile_address , 'r+' ) as f:
-	lines = f.readlines()
-	[ functions.append( re.search('[a-z].*',line).group(0) ) for line in lines if 'function' in line or 'method' in line ]
+		lines = f.readlines()
+		[ functions.append( re.search('[a-z].*',line).group(0) ) for line in lines if 'function' in line or 'method' in line ]
 
 	functions_ = list()
 	[ functions_.append( function ) for function in functions if function[0]=='m' or function[0]=='f' ]
