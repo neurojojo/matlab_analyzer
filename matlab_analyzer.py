51568526793f3982c62c10bbb6c9fb386fad1586
neurojojo <joemesz@gmail.com>
2020-09-27 12:00:40 -0400

    First commit

---
+++ matlab_analyzer.py
@@ -0,0 +1,12 @@
+def dissect_mFile( mfile_address ):
+
+	functions = list()
+	filename = re.search( '(.*\/)([^/]*(?=\.))', mfile_address ).group(2)
+	with open( mfile_address , 'r+' ) as f:
+	lines = f.readlines()
+	[ functions.append( re.search('[a-z].*',line).group(0) ) for line in lines if 'function' in line or 'method' in line ]
+
+	functions_ = list()
+	[ functions_.append( function ) for function in functions if function[0]=='m' or function[0]=='f' ]
+	print( f'Saving to {filename}_architecture.csv' )
+	pd.DataFrame(functions_).to_csv(f"{filename}_architecture.csv")
